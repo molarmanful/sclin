7fe47c698a7b2ca2bfee1f698de92aeb1e262a88
molarmanful <benjinpang@gmail.com>
2022-10-13 18:17:49 -0400

    async example runner

--- docr.js
+++ docr.js
@@ -1,17 +1,28 @@
 let fs = require('fs')
 let cp = require('child_process')
 
-let f = process.argv[2]
-fs.readFile(f, (e, data) => {
+async function replaceAsync(s, r, f) {
+  let all = await Promise.all(Array.from(s.matchAll(r), a => f(...a)))
+  return s.replace(r, _ => all.shift())
+}
+
+let fl = process.argv[2]
+fs.readFile(fl, (e, data) => {
   if (e) throw e
   else {
-    let res = (data + '').replace(/```sclin\s*?\n([^]+?)\n```/g, (_, x) => {
-      let ex = (cp.execSync(`./mill sclin --doceval '${x.replace(/'/g, `'\\''`)}'`) + '').trim().split`\n`
-      let e = ex.pop()
-      let o = '```\n' + `${x}\n${e}${ex.length ? '\n' + ex.join`\n` : ''}` + '\n```'
-      console.log(o)
-      return o
+    replaceAsync(data + '', /```sclin\s*?\n([^]+?)\n```/g, (_, x) => new Promise(resolve => {
+      cp.exec(`./mill sclin --doceval '${x.replace(/'/g, `'\\''`)}'`, (e, res) => {
+        if (e) throw e
+        else {
+          let ex = res.trim().split`\n`
+          let e = ex.pop()
+          let o = '```\n' + `${x}\n${e}${ex.length ? '\n' + ex.join`\n` : ''}` + '\n```'
+          console.log(o)
+          resolve(o)
+        }
+      })
+    })).then(res => {
+      fs.writeFile(fl, res, _ => { })
     })
-    fs.writeFile(f, res, _ => { })
   }
 })
