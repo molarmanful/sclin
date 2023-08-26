b05abb5c93d6acb8e7570a49dcaa985afc419bfb
molarmanful <benjinpang@gmail.com>
2023-08-26 01:24:23 -0400

    small bug

--- docr.js
+++ docr.js
@@ -16,19 +16,28 @@ readFile(fl, (e, data) => {
     replaceAsync(data + '', /```sclin\s*?\n([^]+?)\n```/g, (_, x) => {
       console.log('EXEC:', x)
       return new Promise(resolve => {
-        exec(`out/sclin/assembly.dest/out.jar --doceval '${x.replace(/'/g, `'\\''`)}'`, (e, res) => {
-          if (e) throw e
-          else {
-            let ex = res.trim().split`\n`
-            let e = ex.pop()
-            let o = '```\n' + `${x}\n${e}${ex.length ? '\n' + ex.join`\n` : ''}` + '\n```'
-            console.log('DONE:', o)
-            resolve(o)
+        exec(
+          `out/sclin/jvm/assembly.dest/out.jar --doceval '${x.replace(
+            /'/g,
+            `'\\''`
+          )}'`,
+          (e, res) => {
+            if (e) throw e
+            else {
+              let ex = res.trim().split`\n`
+              let e = ex.pop()
+              let o =
+                '```\n' +
+                `${x}\n${e}${ex.length ? '\n' + ex.join`\n` : ''}` +
+                '\n```'
+              console.log('DONE:', o)
+              resolve(o)
+            }
           }
-        })
+        )
       })
     }).then(res => {
-      writeFile(fl, res, _ => { })
+      writeFile(fl, res, _ => {})
     })
   }
 })
