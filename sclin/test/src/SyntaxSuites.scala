8adeb8d4ac1c036bccbd757f2febc5fa21e90bbf
molarmanful <benjinpang@gmail.com>
2023-10-05 12:26:07 -0400

    name change

--- sclin/test/src/SyntaxSuites.scala
+++ sclin/test/src/SyntaxSuites.scala
@@ -4,14 +4,14 @@ import scala.collection.immutable.VectorMap
 import scala.util.chaining._
 import ANY._
 
-class LambdaSuite extends TU:
+class FNBrackSuite extends TU:
 
   "empty" |? "()" ==> dFN(0)
   "singleton" |? "\\asdf" ==> dFN(0, CMD("asdf"))
   "nested" |? "(()(()))" ==> dFN(0, "()(())".map(_.toString.pipe(CMD(_)))*)
   "open end" |? "(()((" =+> Vector(dFN(0, CMD("("), CMD(")"), CMD("(")), dFN(0))
 
-class ArrBrackSuite extends TU:
+class ARRBrackSuite extends TU:
 
   "empty" |? "[]" ==> UN.toARR
   "nested" |? "[[][[]]]" ==> dARR(dARR(), dARR(dARR()))
