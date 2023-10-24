ecb72f52ea4010a97c83b57449775d89df8bd262
molarmanful <benjinpang@gmail.com>
2023-10-23 23:09:40 -0400

    revert

--- sclin/src/Main.scala
+++ sclin/src/Main.scala
@@ -2,16 +2,13 @@ package sclin
 
 import mainargs._
 import scala.util.chaining._
-import zio._
 
-object Main extends ZIOAppDefault:
+object Main:
 
-  given TokensReader.Simple[os.Path] with
+  def main(args: Array[String]): Unit =
+    ParserForMethods(this).runOrExit(args.toIndexedSeq, allowPositional = true)
 
-    def shortName               = "path"
-    def read(strs: Seq[String]) = Right(os.Path(strs.head, os.pwd))
-
-  @main case class Config(
+  @main def sclin(
       @arg(short = 'f', doc = "Execute file.") file: Option[os.Path],
       @arg(short = 'e', doc = "Execute string.") eval: Option[String],
       @arg(hidden = true) doceval: Option[String],
@@ -19,43 +16,33 @@ object Main extends ZIOAppDefault:
       @arg(short = 'v', doc = "Verbose mode.") verb: Flag,
       @arg(short = 'i', doc = "Implicit mode.") impl: Flag,
       @arg(doc = "Enable/disable ANSI in debug messages.") nocolor: Flag
-  ):
+  ) =
+    def err(e: String) = println(cflag(fansi.Color.Red)(e))
+    def cflag(x: fansi.Attrs) =
+      if nocolor.value then fansi.Attrs() else x
+    try
+      doceval match
+        case Some(s) => ENV.docRun(s)
+        case _ =>
+          val flags = Flags(
+            s = step.value,
+            v = verb.value,
+            i = impl.value,
+            nc = nocolor.value
+          )
+          file match
+            case Some(f) => ENV.run(os.read(f), file, flags, cflag)
+            case _ =>
+              eval match
+                case Some(s) => ENV.run(s, file, flags, cflag)
+                case _       => ()
+    catch
+      case e: java.nio.file.NoSuchFileException =>
+        err(s"no file ${e.getFile()}")
+      case e: LinERR => err(e.toString)
+      case e         => err(s"ERR: $e\n${e.getStackTrace.mkString("\n")}")
 
-    def toFlags: Flags =
-      Flags(s = step.value, v = verb.value, i = impl.value, nc = nocolor.value)
+  given TokensReader.Simple[os.Path] with
 
-  def run =
-    for
-      args <- getArgs
-      cfg <-
-        ZIO.attempt(
-          ParserForClass[Config].constructOrExit(args, allowPositional = true)
-        )
-      _ <-
-        def cflag(x: fansi.Attrs) =
-          if cfg.nocolor.value then fansi.Attrs() else x
-        def err(e: String) = Console.printLine(cflag(fansi.Color.Red)(e))
-
-        cfg.doceval match
-          case Some(s) => ZIO.attemptBlocking(ENV.docRun(s))
-          case _ =>
-            val flags = cfg.toFlags
-            try
-              cfg.file match
-                case Some(f) =>
-                  ZIO.attempt(
-                    ENV.run(os.read(f), cfg.file, flags, cflag)
-                  )
-                case _ =>
-                  cfg.eval match
-                    case Some(s) =>
-                      ZIO.attempt(
-                        ENV.run(s, cfg.file, flags, cflag)
-                      )
-                    case _ => ZIO.unit
-            catch
-              case e: java.nio.file.NoSuchFileException =>
-                err(s"no file ${e.getFile()}")
-              case e: LinERR => err(e.toString)
-              case e         => err(s"ERR: $e\n${e.getStackTrace.mkString("\n")}")
-    yield ()
+    def shortName               = "path"
+    def read(strs: Seq[String]) = Right(os.Path(strs.head, os.pwd))
