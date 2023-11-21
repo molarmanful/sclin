fff17cfe017cf1cc0f25f7dcb6ed032938ba1896
molarmanful <benjinpang@gmail.com>
2023-11-21 03:08:22 -0500

    test

--- sclin/test/src/SyntaxSuites.scala
+++ sclin/test/src/SyntaxSuites.scala
@@ -8,7 +8,8 @@ class FNBrackSuite extends TU:
   "empty" |? "()" ==> dFN(0)
   "singleton" |? "\\asdf" ==> dFN(0, CMD("asdf"))
   "nested" |? "(()(()))" ==> dFN(0, "()(())".map(_.toString.pipe(CMD(_)))*)
-  "open end" |? "(()((" =+> Vector(dFN(0, CMD("("), CMD(")"), CMD("(")), dFN(0))
+  "open end" |? "(()((" =+> Vector:
+    dFN(0, CMD("("), CMD(")"), CMD("("), CMD("("))
 
 class ARRBrackSuite extends TU:
 
